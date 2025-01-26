8f9c8d8419a940658c806df900153eda7f9fa8b6
Hesham Elsayed <93478673+Hesham749@users.noreply.github.com>
2025-01-26 05:19:30 -0800

    initial commit

---
+++ SchemaScript.sql
@@ -0,0 +1,413 @@
+CREATE TABLE Branch
+(
+  ID    int          NOT NULL IDENTITY(1,1),
+  Phone varchar(13) ,
+  City  nvarchar(20) NOT NULL,
+  CONSTRAINT PK_Branch PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE Branch
+  ADD CONSTRAINT UQ_BranchPhone UNIQUE (Phone)
+GO
+
+ALTER TABLE Branch
+  ADD CONSTRAINT UQ_BranchCity UNIQUE (City)
+GO
+
+CREATE TABLE BranchTracks
+(
+  BranchID int NOT NULL,
+  TrackID  int NOT NULL,
+  CONSTRAINT PK_BranchTracks PRIMARY KEY (BranchID, TrackID)
+)
+GO
+
+CREATE TABLE Course
+(
+  ID       int         NOT NULL IDENTITY(1,1),
+  Name     varchar(50) NOT NULL,
+  Duration smallint    NOT NULL,
+  CONSTRAINT PK_Course PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE Course
+  ADD CONSTRAINT UQ_CourseName UNIQUE (Name)
+GO
+
+CREATE TABLE CoursesInstructors
+(
+  CrsID int NOT NULL,
+  InsID int NOT NULL,
+  CONSTRAINT PK_CoursesInstructors PRIMARY KEY (CrsID, InsID)
+)
+GO
+
+CREATE TABLE Exam
+(
+  ID            int           NOT NULL IDENTITY(1,1),
+  Name          varchar(50)   NOT NULL,
+  StartTime     datetime      NOT NULL,
+  Duration      DECIMAL(3,2)  NOT NULL,
+  EndTime       AS            DATEADD(hour , Duration , StartTime),
+  QuestionCount tinyint       NOT NULL,
+  TotalMark     tinyint       DEFAULT  0 ,
+  CrsID         int           NOT NULL,
+  InsID         int           NOT NULL,
+  CONSTRAINT PK_Exam PRIMARY KEY (ID)
+)
+GO
+
+CREATE TABLE ExamQuestions
+(
+  ExamID     int NOT NULL,
+  QuestionID int NOT NULL,
+  CONSTRAINT PK_ExamQuestions PRIMARY KEY (ExamID, QuestionID)
+)
+GO
+
+CREATE TABLE Instructor
+(
+  ID       int          NOT NULL IDENTITY(1,1),
+  Fname    nvarchar(30) NOT NULL,
+  Lname    nvarchar(30)         ,
+  BD       date         NOT NULL,
+  Age      AS           YEAR(GETDATE()) - YEAR(BD),
+  Gender   varchar(1)   NOT NULL CHECK (Gender in ('m','f')),
+  Email    varchar(50)  NOT NULL,
+  Password varchar(10)  NOT NULL,
+  St       nvarchar(50) NOT NULL,
+  City     nvarchar(50) NOT NULL,
+  Phone    varchar(13)  NOT NULL,
+  HireDate date         NOT NULL DEFAULT GETDATE(),
+  Salary   decimal(8,2) NOT NULL,
+  SuperID  int                  ,
+  CONSTRAINT PK_Instructor PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE Instructor
+  ADD CONSTRAINT UQ_InstructorEmail UNIQUE (Email)
+GO
+
+ALTER TABLE Instructor
+  ADD CONSTRAINT UQ_InstructorPhone UNIQUE (Phone)
+GO
+
+CREATE TABLE Intake
+(
+  ID        int         NOT NULL IDENTITY(1,1),
+  Name      varchar(30) NOT NULL,
+  StartDate date        NOT NULL,
+  EndDate   date        NOT NULL,
+  CONSTRAINT PK_Intake PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE Intake
+  ADD CONSTRAINT UQ_IntakeName UNIQUE (Name)
+GO
+
+CREATE TABLE Question
+(
+  ID            int          NOT NULL IDENTITY(1,1),
+  Body          varchar(150) NOT NULL,
+  Mark          tinyint      NOT NULL,
+  CorrectAnswer tinyint              ,
+  TypeID        int          NOT NULL,
+  CrsID         int          NOT NULL,
+  InsID         int          NOT NULL,
+  CONSTRAINT PK_Question PRIMARY KEY (ID)
+)
+GO
+
+CREATE TABLE QuestionOptions
+(
+  QuestionID int         NOT NULL,
+  OptionNum  tinyint     NOT NULL CHECK(OptionNum BETWEEN 1 AND 4),
+  OptionBody varchar(50) NOT NULL,
+  CONSTRAINT PK_QuestionOptions PRIMARY KEY (QuestionID, OptionNum)
+)
+GO
+
+CREATE TABLE QuestionTypes
+(
+  ID   int          NOT NULL IDENTITY(1,1),
+  Type nvarchar(30) NOT NULL,
+  CONSTRAINT PK_QuestionTypes PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE QuestionTypes
+  ADD CONSTRAINT UQ_Type UNIQUE (Type)
+GO
+
+CREATE TABLE Student
+(
+  ID       int          NOT NULL IDENTITY(1,1),
+  Fname    nvarchar(30) NOT NULL,
+  Lname    nvarchar(30),
+  BD       date         NOT NULL,
+  Age      AS           YEAR(GETDATE()) - YEAR(BD),
+  Gender   varchar(1)   NOT NULL CHECK (Gender in ('m','f')),
+  Email    varchar(50)  NOT NULL,
+  Password varchar(10)  NOT NULL,
+  St       nvarchar(50) NOT NULL,
+  City     nvarchar(50) NOT NULL,
+  Phone    varchar(13)  NOT NULL,
+  IntakeID int          NOT NULL,
+  TrackID  int          NOT NULL,
+  CONSTRAINT PK_Student PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE Student
+  ADD CONSTRAINT UQ_StudentEmail UNIQUE (Email)
+GO
+
+ALTER TABLE Student
+  ADD CONSTRAINT UQ_StudentPhone UNIQUE (Phone)
+GO
+
+CREATE TABLE StudentCourses
+(
+  StdID int NOT NULL,
+  CrsID int NOT NULL,
+  CONSTRAINT PK_StudentCourses PRIMARY KEY (StdID, CrsID)
+)
+GO
+
+CREATE TABLE StudentExams
+(
+  StdID  int     NOT NULL,
+  ExamID int     NOT NULL,
+  Grade  tinyint DEFAULT 0 NOT NULL,
+  CONSTRAINT PK_StudentExams PRIMARY KEY (StdID, ExamID)
+)
+GO
+
+CREATE TABLE StudentsExamsAnswers
+(
+  StdID       int     NOT NULL,
+  QuestionID  int     NOT NULL,
+  ExamID      int     NOT NULL,
+  StdAnswer   tinyint         ,
+  AnswerGrade tinyint         ,
+  CONSTRAINT PK_StudentsExamsAnswers PRIMARY KEY (StdID, QuestionID, ExamID)
+)
+GO
+
+CREATE TABLE Topic
+(
+  ID    int         NOT NULL IDENTITY(1,1),
+  CrsID int         NOT NULL,
+  Name  varchar(50) NOT NULL,
+  CONSTRAINT PK_Topic PRIMARY KEY (ID)
+)
+GO
+
+CREATE TABLE Track
+(
+  ID       int          NOT NULL IDENTITY(1,1),
+  Name     nvarchar(30) NOT NULL,
+  IntakeID int          NOT NULL,
+  MngrID   int          NOT NULL,
+  HireDate date         NOT NULL DEFAULT GETDATE(),
+  CONSTRAINT PK_Track PRIMARY KEY (ID)
+)
+GO
+
+ALTER TABLE Track
+  ADD CONSTRAINT UQ_TrackName UNIQUE (Name)
+GO
+
+CREATE TABLE TrackCourses
+(
+  TrackID int NOT NULL,
+  CrsID   int NOT NULL,
+  CONSTRAINT PK_TrackCourses PRIMARY KEY (TrackID, CrsID)
+)
+GO
+
+CREATE TABLE TrackInstructors
+(
+  TrackID int NOT NULL,
+  InsID   int NOT NULL,
+  CONSTRAINT PK_TrackInstructors PRIMARY KEY (TrackID, InsID)
+)
+GO
+
+ALTER TABLE BranchTracks
+  ADD CONSTRAINT FK_Branch_TO_BranchTracks
+    FOREIGN KEY (BranchID)
+    REFERENCES Branch (ID)
+GO
+
+ALTER TABLE BranchTracks
+  ADD CONSTRAINT FK_Track_TO_BranchTracks
+    FOREIGN KEY (TrackID)
+    REFERENCES Track (ID)
+GO
+
+ALTER TABLE Track
+  ADD CONSTRAINT FK_Intake_TO_Track
+    FOREIGN KEY (IntakeID)
+    REFERENCES Intake (ID)
+GO
+
+ALTER TABLE Student
+  ADD CONSTRAINT FK_Intake_TO_Student
+    FOREIGN KEY (IntakeID)
+    REFERENCES Intake (ID)
+GO
+
+ALTER TABLE Student
+  ADD CONSTRAINT FK_Track_TO_Student
+    FOREIGN KEY (TrackID)
+    REFERENCES Track (ID)
+GO
+
+ALTER TABLE StudentCourses
+  ADD CONSTRAINT FK_Course_TO_StudentCourses
+    FOREIGN KEY (CrsID)
+    REFERENCES Course (ID)
+GO
+
+ALTER TABLE StudentCourses
+  ADD CONSTRAINT FK_Student_TO_StudentCourses
+    FOREIGN KEY (StdID)
+    REFERENCES Student (ID)
+GO
+
+ALTER TABLE TrackCourses
+  ADD CONSTRAINT FK_Course_TO_TrackCourses
+    FOREIGN KEY (CrsID)
+    REFERENCES Course (ID)
+GO
+
+ALTER TABLE TrackCourses
+  ADD CONSTRAINT FK_Track_TO_TrackCourses
+    FOREIGN KEY (TrackID)
+    REFERENCES Track (ID)
+GO
+
+ALTER TABLE Question
+  ADD CONSTRAINT FK_QuestionTypes_TO_Question
+    FOREIGN KEY (TypeID)
+    REFERENCES QuestionTypes (ID)
+GO
+
+ALTER TABLE QuestionOptions
+  ADD CONSTRAINT FK_Question_TO_QuestionOptions
+    FOREIGN KEY (QuestionID)
+    REFERENCES Question (ID)
+GO
+
+ALTER TABLE StudentsExamsAnswers
+  ADD CONSTRAINT FK_Student_TO_StudentsExamsAnswers
+    FOREIGN KEY (StdID)
+    REFERENCES Student (ID)
+GO
+
+ALTER TABLE StudentsExamsAnswers
+  ADD CONSTRAINT FK_Question_TO_StudentsExamsAnswers
+    FOREIGN KEY (QuestionID)
+    REFERENCES Question (ID)
+GO
+
+ALTER TABLE ExamQuestions
+  ADD CONSTRAINT FK_Exam_TO_ExamQuestions
+    FOREIGN KEY (ExamID)
+    REFERENCES Exam (ID)
+GO
+
+ALTER TABLE ExamQuestions
+  ADD CONSTRAINT FK_Question_TO_ExamQuestions
+    FOREIGN KEY (QuestionID)
+    REFERENCES Question (ID)
+GO
+
+ALTER TABLE StudentExams
+  ADD CONSTRAINT FK_Student_TO_StudentExams
+    FOREIGN KEY (StdID)
+    REFERENCES Student (ID)
+GO
+
+ALTER TABLE StudentExams
+  ADD CONSTRAINT FK_Exam_TO_StudentExams
+    FOREIGN KEY (ExamID)
+    REFERENCES Exam (ID)
+GO
+
+ALTER TABLE Exam
+  ADD CONSTRAINT FK_Course_TO_Exam
+    FOREIGN KEY (CrsID)
+    REFERENCES Course (ID)
+GO
+
+ALTER TABLE Track
+  ADD CONSTRAINT FK_Instructor_TO_Track
+    FOREIGN KEY (MngrID)
+    REFERENCES Instructor (ID)
+GO
+
+ALTER TABLE Instructor
+  ADD CONSTRAINT FK_Instructor_TO_Instructor
+    FOREIGN KEY (SuperID)
+    REFERENCES Instructor (ID)
+GO
+
+ALTER TABLE TrackInstructors
+  ADD CONSTRAINT FK_Instructor_TO_TrackInstructors
+    FOREIGN KEY (InsID)
+    REFERENCES Instructor (ID)
+GO
+
+ALTER TABLE TrackInstructors
+  ADD CONSTRAINT FK_Track_TO_TrackInstructors
+    FOREIGN KEY (TrackID)
+    REFERENCES Track (ID)
+GO
+
+ALTER TABLE CoursesInstructors
+  ADD CONSTRAINT FK_Course_TO_CoursesInstructors
+    FOREIGN KEY (CrsID)
+    REFERENCES Course (ID)
+GO
+
+ALTER TABLE CoursesInstructors
+  ADD CONSTRAINT FK_Instructor_TO_CoursesInstructors
+    FOREIGN KEY (InsID)
+    REFERENCES Instructor (ID)
+GO
+
+ALTER TABLE Question
+  ADD CONSTRAINT FK_Instructor_TO_Question
+    FOREIGN KEY (InsID)
+    REFERENCES Instructor (ID)
+GO
+
+ALTER TABLE Exam
+  ADD CONSTRAINT FK_Instructor_TO_Exam
+    FOREIGN KEY (InsID)
+    REFERENCES Instructor (ID)
+GO
+
+ALTER TABLE Question
+  ADD CONSTRAINT FK_Course_TO_Question
+    FOREIGN KEY (CrsID)
+    REFERENCES Course (ID)
+GO
+
+ALTER TABLE Topic
+  ADD CONSTRAINT FK_Course_TO_Topic
+    FOREIGN KEY (CrsID)
+    REFERENCES Course (ID)
+GO
+
+ALTER TABLE StudentsExamsAnswers
+  ADD CONSTRAINT FK_Exam_TO_StudentsExamsAnswers
+    FOREIGN KEY (ExamID)
+    REFERENCES Exam (ID)
+GO
---
+++ Triggers.sql
@@ -0,0 +1,239 @@
+use ExaminationSystem
+
+
+--! question
+
+--* insert
+
+CREATE TRIGGER trg_InsteadOfInsert
+ON question
+INSTEAD OF INSERT
+AS
+ BEGIN
+DECLARE  @CrsID INT , @InsID  INT
+
+SELECT  @CrsID = i.CrsID , @InsID = i.InsID  FROM inserted AS i
+
+IF Exists (SELECT ci.CrsID FROM CoursesInstructors AS ci WHERE ci.CrsID = @CrsID AND ci.InsID = @InsID )
+    BEGIN
+        INSERT INTO question(Body, Mark,CorrectAnswer,TypeID , CrsID , InsID)
+        SELECT Body, Mark,CorrectAnswer,TypeID , CrsID , InsID FROM inserted
+    END
+ELSE
+RAISERROR('operation failed',12,1)
+
+END;
+
+--* update
+
+ALTER TRIGGER trg_QuestionAfterUpdate
+ON question
+After UPDATE
+AS
+ BEGIN
+    DECLARE  @ID INT , @CrsID INT , @InsID  INT
+
+        SELECT  @ID = ID , @InsID = InsID ,  @CrsID = CrsID   FROM inserted
+        IF Exists (SELECT 1 FROM ExamQuestions AS ea WHERE ea.QuestionID = @ID )
+            BEGIN
+                SELECT @CrsID = CrsID , @InsID = InsID  FROM deleted ;
+                    UPDATE question
+                    SET CrsID = @CrsID , InsID = @InsID
+                    WHERE ID =@ID
+                RAISERROR('not allowed to update question that already in students exams',14,1)
+            END
+        ELSE IF UPDATE(CrsID) OR  UPDATE(InsID)
+            BEGIN
+                IF NOT  Exists (SELECT ci.CrsID FROM CoursesInstructors AS ci WHERE ci.CrsID = @CrsID AND ci.InsID = @InsID )
+                    BEGIN
+                        SELECT @CrsID = CrsID , @InsID = InsID  FROM deleted ;
+                            UPDATE question
+                            SET CrsID = @CrsID , InsID = @InsID
+                            WHERE ID =@ID
+                    END
+                    ELSE
+                        RAISERROR('operation failed',13,1)
+            END
+END;
+
+--! correct answer
+
+--* insert
+
+CREATE TRIGGER trg_AfterInsertCorrectAnswer
+ON question
+After INSERT
+AS
+ BEGIN
+    DECLARE  @ID INT , @CorrectAnswer INT
+     SELECT @ID = ID , @CorrectAnswer = CorrectAnswer FROM inserted
+        IF NOT  Exists (SELECT qa.OptionNum FROM QuestionOptions AS qa WHERE qa.OptionNum = @CorrectAnswer AND qa.QuestionID = @ID )
+            BEGIN
+                SET @CorrectAnswer = NULL ;
+                UPDATE question
+                SET CorrectAnswer = @CorrectAnswer
+                WHERE ID =@ID
+            END
+            ELSE
+            print 'correct answer st to NULL';
+END;
+
+--* update
+
+create TRIGGER trg_AfterUpdateCorrectAnswer
+ON question
+After UPDATE
+AS
+ BEGIN
+    DECLARE  @ID INT , @CorrectAnswer INT
+     SELECT @ID = ID , @CorrectAnswer = CorrectAnswer FROM inserted
+
+ IF UPDATE(CorrectAnswer)
+    BEGIN
+        IF NOT  Exists (SELECT qa.OptionNum FROM QuestionOptions AS qa WHERE qa.OptionNum = @CorrectAnswer AND qa.QuestionID = @ID )
+            BEGIN
+                     SELECT @CorrectAnswer = CorrectAnswer FROM deleted ;
+                UPDATE question
+                SET CorrectAnswer = @CorrectAnswer
+                WHERE ID =@ID
+            END
+        ELSE
+            print 'your input is not exist in question options';
+    END
+
+END;
+
+
+--! question exam
+
+
+--* insert
+
+ALTER TRIGGER trg_ExamQuestionInsteadOfInsert
+ON ExamQuestions
+INSTEAD OF INSERT
+AS
+ BEGIN
+DECLARE  @QuesID INT , @ExamID INT ,@QuesCount int = NULL, @quesCrs INT , @QesMark INT , @HasOption BINARY
+
+SELECT  @QuesID = i.QuestionID , @ExamID = i.ExamID  FROM inserted AS i
+SELECT @quesCrs = CrsID , @QesMark = Mark FROM question WHERE ID = @QuesID
+SELECT @QuesCount = e.QuestionCount FROM exam as e WHERE e.CrsID = @QuesCrs AND ID = @ExamID
+ set @HasOption = IIF( exists(select 1 FROM QuestionOptions WHERE QuestionID = @QuesID) , 1 , 0)
+IF  ( @QuesCount IS NOT NULL)
+    BEGIN
+        IF (SELECT COUNT(*) FROM ExamQuestions WHERE ExamID = @ExamID) < @QuesCount AND  @HasOption = 1
+        BEGIN
+            BEGIN TRY
+                BEGIN TRANSACTION
+                    INSERT INTO ExamQuestions(ExamID , QuestionID)
+                        VALUES (@ExamID, @QuesID)
+
+                            UPDATE Exam
+                            SET TotalMark += @QesMark
+                            WHERE ID = @ExamID
+                        COMMIT TRANSACTION
+            END TRY
+            BEGIN CATCH
+                ROLLBACK TRANSACTION
+                print 'operation failed'
+            END CATCH
+        END
+        ELSE IF @HasOption = 0
+            RAISERROR('Question does not have options',12,1)
+        ELSE
+            RAISERROR('exam questions is full',12,1)
+    END
+ELSE
+RAISERROR('operation failed',12,1)
+
+END;
+
+--* update
+
+ALTER TRIGGER trg_ExamQuestionAfterUpdate
+ON ExamQuestions
+    After UPDATE
+AS
+BEGIN
+    DECLARE  @QuesID INT , @ExamID INT , @QuesCrs INT , @QuesMark INT , @NewQuesID INT , @NewExamID INT , @NewQuesMark INT
+    SELECT   @ExamID = ExamID , @QuesID = QuestionID  FROM deleted AS e
+    SELECT  @NewQuesID = i.QuestionID , @NewExamID = ExamID FROM inserted AS i
+    SELECT @QuesCrs = CrsID , @NewQuesMark = Mark FROM question WHERE ID = @NewQuesID
+     IF @ExamID != @NewExamID
+            BEGIN
+                    UPDATE ExamQuestions
+                    Set ExamID = @ExamID, QuestionID = @QuesID
+                        WHERE ExamID = @NewExamID AND QuestionID = @NewQuesID;
+                 RAISERROR('You can not change Exam' ,16, 1)
+            END
+    ELSE IF NOT EXISTS(SELECT 1 FROM exam as e WHERE e.CrsID = @QuesCrs AND ID = @ExamID)
+        BEGIN
+                UPDATE ExamQuestions
+                    Set ExamID = @ExamID, QuestionID = @QuesID
+                        WHERE ExamID = @NewExamID AND QuestionID = @NewQuesID;
+                 IF Exists(SELECT 1 FROM StudentExams WHERE ExamID = @ExamID)
+                    BEGIN
+                        RAISERROR('Exam already launched' ,16, 1)
+                        RETURN ;
+                    END
+        END
+
+    ELSE
+     BEGIN
+      SELECT  @QuesMark = Mark  FROM question WHERE ID = @QuesID
+                UPDATE Exam
+                    SET TotalMark +=  (@NewQuesMark - @QuesMark )
+                    where ID = @ExamID;
+     END
+
+END;
+
+
+--! StudentsExamsAnswers
+
+--* insert
+
+CREATE TRIGGER trg_StudentsExamsAnswersAfterInsert
+ON StudentsExamsAnswers
+After INSERT
+AS
+BEGIN
+    DECLARE @stdID INT , @QuesID INT , @ExamID INT , @StdAnswer INT , @AnswerGrade TINYINT
+    select @stdID = StdID , @QuesID = QuestionID , @ExamID = ExamID , @StdAnswer = StdAnswer  FROM inserted
+    IF NOT Exists(select 1 FROM StudentExams as se JOIN ExamQuestions as eq on eq.ExamID = se.ExamID AND  eq.ExamID = @ExamID  AND se.StdID = @stdID AND eq.QuestionID = @QuesID)
+            OR NOT EXISTS(select 1 FROM QuestionOptions WHERE QuestionID = @QuesID AND OptionNum = @StdAnswer)
+        BEGIN
+            ROLLBACK
+            RAISERROR('Wrong inputs',13,1)
+            RETURN;
+        END
+    ELSE
+        BEGIN
+            SELECT @AnswerGrade = IIF(@StdAnswer = CorrectAnswer , Mark , 0) FROM question as q WHERE q.ID = @QuesID
+            begin TRY
+                BEGIN TRANSACTION
+                    UPDATE StudentsExamsAnswers
+                        SET AnswerGrade = @AnswerGrade
+                        WHERE StdID = @stdID AND QuestionID = @QuesID AND ExamID = @ExamID ;
+
+                    UPDATE StudentExams
+                        SET Grade += @AnswerGrade
+                        WHERE StdID = @stdID AND ExamID = @ExamID
+
+                    COMMIT TRANSACTION
+            end TRY
+            BEGIN CATCH
+                PRINT 'Operation Failed'
+                ROLLBACK TRANSACTION
+            END CATCH
+        END
+
+END;
+
+
+
+--! Exam
+
+--* insert
+
