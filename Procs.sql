cb1869d236a9dcde019708a4be1d1a320d9fe50d
Hesham749 <heesham749@gmail.com>
2025-01-29 18:43:36 +0200

    Refactor Procs.sql to improve readability and consistency by adding missing GO statements and adjusting comments

--- Procs.sql
+++ Procs.sql
@@ -1,131 +1,131 @@
 USE ExaminationSystem
 
---Student
+--!Student
+
+-- GO
+-- CREATE PROCEDURE sp_InsertStudent
+--     @Fname NVARCHAR(30) ,
+--     @Lname NVARCHAR(30),
+--     @BD DATE,
+--     @Gender VARCHAR(1),
+--     @Email VARCHAR(50),
+--     @Password VARCHAR(10),
+--     @st NVARCHAR(50),
+--     @city VARCHAR(50),
+--     @Phone VARCHAR(13),
+--     @IntakeID INT,
+--     @TrackID INT
+-- AS
+-- BEGIN
+--     BEGIN TRY
+--         BEGIN TRANSACTION;
+
+--         INSERT INTO Student (Fname, Lname, BD, Gender, Email, Password, st, city, Phone, IntakeID, TrackID)
+--         VALUES (@Fname, @Lname, @BD, @Gender, @Email, @Password, @st, @city, @Phone, @IntakeID, @TrackID);
+
+--         COMMIT TRANSACTION;
+--     END TRY
+--     BEGIN CATCH
+--         ROLLBACK TRANSACTION;
+--         PRINT 'Student did not insert' ;
+--     END CATCH
+-- END;
+
+-- GO
+
+-- CREATE PROC sp_UpdateStudent
+--     @ID INT ,
+--     @Fname NVARCHAR(30) = NULL,
+--     @Lname NVARCHAR(30) =NULL,
+--     @BD DATE =NULL,
+--     @Gender VARCHAR(1) =NULL,
+--     @Email VARCHAR(50) =NULL,
+--     @Password VARCHAR(10) =NULL,
+--     @st NVARCHAR(50) =NULL,
+--     @city VARCHAR(50) =NULL,
+--     @Phone VARCHAR(13) =NULL,
+--     @IntakeID INT =NULL,
+--     @TrackID INT =NULL
+--     AS
+--     BEGIN
+
+--    BEGIN TRY
+--         BEGIN TRANSACTION;
+
+--         UPDATE Student
+--         SET
+--          Fname = ISNULL(@Fname,Fname),
+--          Lname = ISNULL(@Lname,Lname),
+--          BD = ISNULL(@BD,BD),
+--          Gender = ISNULL(@Gender,Gender),
+--          Email = ISNULL(@Email,Email),
+--          Password = ISNULL(@Password,Password),
+--          St = ISNULL(@St,St),
+--          City = ISNULL(@City,City),
+--          Phone = ISNULL(@Phone,Phone),
+--          IntakeID = ISNULL(@IntakeID,IntakeID),
+--          TrackID = ISNULL(@TrackID,TrackID)
+--          WHERE ID = @ID
+
+--         COMMIT TRANSACTION;
+--     END TRY
+--     BEGIN CATCH
+--         ROLLBACK TRANSACTION;
+--         PRINT 'Student did not Update' ;
+--     END CATCH
+
+--     END;
+
+-- GO
+
+-- CREATE PROC sp_DeleteStudent @ID int
+-- AS
+-- BEGIN
+--  BEGIN TRY
+--         BEGIN TRANSACTION;
+
+--         DELETE Student
+--          WHERE ID = @ID
+
+--         COMMIT TRANSACTION;
+--     END TRY
+--     BEGIN CATCH
+--         ROLLBACK TRANSACTION;
+--         PRINT 'Student did not delete';
+--     END CATCH
+-- END;
+
+-- GO
+
+
+-- -- question
+
+-- CREATE PROC sp_InsertQuestion
+-- @Body VARCHAR(150),
+-- @Mark TINYINT ,
+-- @CrsID int ,
+-- @TypeID int ,
+-- @InsID  int
+-- AS
+-- BEGIN
+
+--  BEGIN TRY
+--         BEGIN TRANSACTION;
+
+--         INSERT INTO Question (Body, Mark, CrsID, TypeID, InsID)
+--         VALUES ( @Body, @Mark, @CrsID, @TypeID, @InsID);
+
+--         COMMIT TRANSACTION;
+--     END TRY
+--     BEGIN CATCH
+--         ROLLBACK TRANSACTION;
+--         PRINT 'question did not insert';
+--     END CATCH
+
+-- END;
+
 
 GO
-CREATE PROCEDURE sp_InsertStudent
-    @Fname NVARCHAR(30) ,
-    @Lname NVARCHAR(30),
-    @BD DATE,
-    @Gender VARCHAR(1),
-    @Email VARCHAR(50),
-    @Password VARCHAR(10),
-    @st NVARCHAR(50),
-    @city VARCHAR(50),
-    @Phone VARCHAR(13),
-    @IntakeID INT,
-    @TrackID INT
-AS
-BEGIN
-    BEGIN TRY
-        BEGIN TRANSACTION;
-
-        INSERT INTO Student (Fname, Lname, BD, Gender, Email, Password, st, city, Phone, IntakeID, TrackID)
-        VALUES (@Fname, @Lname, @BD, @Gender, @Email, @Password, @st, @city, @Phone, @IntakeID, @TrackID);
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION;
-        PRINT 'Student did not insert' ;
-    END CATCH
-END;
-
-GO
-
-CREATE PROC sp_UpdateStudent
-    @ID INT ,
-    @Fname NVARCHAR(30) = NULL,
-    @Lname NVARCHAR(30) =NULL,
-    @BD DATE =NULL,
-    @Gender VARCHAR(1) =NULL,
-    @Email VARCHAR(50) =NULL,
-    @Password VARCHAR(10) =NULL,
-    @st NVARCHAR(50) =NULL,
-    @city VARCHAR(50) =NULL,
-    @Phone VARCHAR(13) =NULL,
-    @IntakeID INT =NULL,
-    @TrackID INT =NULL
-    AS
-    BEGIN
-
-   BEGIN TRY
-        BEGIN TRANSACTION;
-
-        UPDATE Student
-        SET
-         Fname = ISNULL(@Fname,Fname),
-         Lname = ISNULL(@Lname,Lname),
-         BD = ISNULL(@BD,BD),
-         Gender = ISNULL(@Gender,Gender),
-         Email = ISNULL(@Email,Email),
-         Password = ISNULL(@Password,Password),
-         St = ISNULL(@St,St),
-         City = ISNULL(@City,City),
-         Phone = ISNULL(@Phone,Phone),
-         IntakeID = ISNULL(@IntakeID,IntakeID),
-         TrackID = ISNULL(@TrackID,TrackID)
-         WHERE ID = @ID
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION;
-        PRINT 'Student did not Update' ;
-    END CATCH
-
-    END;
-
-GO
-
-CREATE PROC sp_DeleteStudent @ID int
-AS
-BEGIN
- BEGIN TRY
-        BEGIN TRANSACTION;
-
-        DELETE Student
-         WHERE ID = @ID
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION;
-        PRINT 'Student did not delete';
-    END CATCH
-END;
-
-GO
-
-
--- question
-
-CREATE PROC sp_InsertQuestion
-@Body VARCHAR(150),
-@Mark TINYINT ,
-@CrsID int ,
-@TypeID int ,
-@InsID  int
-AS
-BEGIN
-
- BEGIN TRY
-        BEGIN TRANSACTION;
-
-        INSERT INTO Question (Body, Mark, CrsID, TypeID, InsID)
-        VALUES ( @Body, @Mark, @CrsID, @TypeID, @InsID);
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION;
-        PRINT 'question did not insert';
-    END CATCH
-
-END;
-
-
-
 
 
 /*
@@ -217,3 +217,5 @@ BEGIN
         END
 
 END;
+
+GO
